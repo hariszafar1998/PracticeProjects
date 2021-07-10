a6d8145f0fb09ab6608430e20ca0c02ac18ea640
hariszafar1998 <86205356+hariszafar1998@users.noreply.github.com>
2021-07-11 01:04:35 +0500

    project done

---
+++ ColorPaleteGenerator/script.js
@@ -0,0 +1,45 @@
+// Get DOM elements
+const color1 = document.getElementById('color1');
+const color2 = document.getElementById('color2');
+const color3 = document.getElementById('color3');
+const color4 = document.getElementById('color4');
+const color5 = document.getElementById('color5');
+const color1Code = document.getElementById('color1Code');
+const color2Code = document.getElementById('color2Code');
+const color3Code = document.getElementById('color3Code');
+const color4Code = document.getElementById('color4Code');
+const color5Code = document.getElementById('color5Code');
+const mainContainer = document.querySelector('mainContainer');
+
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+
+        // Generate 5 random colors
+
+        const randomColor1 = Math.floor(Math.random()*16777215).toString(16);
+        // console.log(randomColor1);
+        const randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+        // console.log(randomColor2);
+        const randomColor3 = Math.floor(Math.random()*16777215).toString(16);
+        // console.log(randomColor3);
+        const randomColor4 = Math.floor(Math.random()*16777215).toString(16);
+        // console.log(randomColor4);
+        const randomColor5 = Math.floor(Math.random()*16777215).toString(16);
+        console.log(randomColor5);
+
+        // Update the DIVs with randomly generated colors and also update the color code
+
+        color1.style.background = `#${randomColor1}`;
+        color1Code.innerText = randomColor1;
+        color2.style.background = `#${randomColor2}`;
+        color2Code.innerText = randomColor2;
+        color3.style.background = `#${randomColor3}`;
+        color3Code.innerText = randomColor3;
+        color4.style.background = `#${randomColor4}`;
+        color4Code.innerText = randomColor4;
+        color5.style.background = `#${randomColor5}`;
+        color5Code.innerText = randomColor5;
+    }
+}
+
+mainContainer.addEventListener('click', e);
